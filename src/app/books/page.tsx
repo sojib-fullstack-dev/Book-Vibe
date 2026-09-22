e1d23870188c9edbb48a4a9d564e4256fd22fe2c
samiulhasansojibe <samiulhasansojib069@gmail.comm>
2026-09-22 12:26:23 +0600

    fix: updated

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -5,7 +5,7 @@ import { IBook } from "@/types/book.type";
 import BookCard from "../components/BookCard";
 
 const getBooks = async () => {
-  const res = await fetch("http://localhost:3000/booksData.json");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`);
   const data = await res.json();
   return data;
 };
