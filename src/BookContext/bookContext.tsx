edddf74030d29edaa2e76a5b859d7b2d470dad32
samiulhasansojibe <samiulhasansojib069@gmail.comm>
2026-09-22 08:15:13 +0600

    Type Issue solve

--- src/BookContext/bookContext.tsx
+++ src/BookContext/bookContext.tsx
@@ -1,14 +1,24 @@
 "use client"
+import { IBook } from '@/types/book.type';
 import React, { createContext, ReactNode, useState } from 'react';
 
+interface IBooksContext {
+  readBooks: IBook[];
+  setReadBooks: React.Dispatch<React.SetStateAction<IBook[]>>;
+  wishlist: IBook[];
+  setWishlist: React.Dispatch<React.SetStateAction<IBook[]>>;
+}
 
-export const BookContext = createContext({})
+export const BookContext = createContext<IBooksContext>({readBooks: [],
+  setReadBooks: () => {},
+  wishlist: [],
+  setWishlist: () => {},})
 
 
 const BookProvider = ({ children }: { children: ReactNode }) => {
 
-    const [readBooks, setReadBooks] = useState([]);
-    const [wishlist, setWishlist] = useState([]);
+    const [readBooks, setReadBooks] = useState<IBook[]>([]);
+    const [wishlist, setWishlist] = useState <IBook[]>([]);
 
     const sharedData = {
         readBooks,
