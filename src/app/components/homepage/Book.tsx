e8e74ecd8f877bf6bec7512541f17e526e12f220
samiulhasansojibe <samiulhasansojib069@gmail.comm>
2026-09-22 12:42:49 +0600

    Type Issue solve

--- src/app/components/homepage/Book.tsx
+++ src/app/components/homepage/Book.tsx
@@ -8,7 +8,7 @@ const getBooks = async () => {
 
     try {
 
-        const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`);
+        const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`,{cache:'no-store'});
 
         const data = await res.json();
 
